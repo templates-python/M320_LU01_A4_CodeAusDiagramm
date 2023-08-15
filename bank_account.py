5e67b007d54a8a741842e834d7eb0566c9096d59
Rene Probst <rene.probst@bzz.ch>
2023-08-15 14:58:06 +0200

    solution add

--- bank_account.py
+++ bank_account.py
@@ -1,14 +1,33 @@
 class BankAccount:
-    '''
-    fügen Sie hier als Erstes den Konstruktor ein.
-    '''
+
+    def __init__(self, max_overdraft, customer):
+        self.__balance   = 0.0
+        self.__overdraft = max_overdraft
+        self.__customer  = customer
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def overdraft(self):
+        return self.__overdraft
+
+    @property
+    def customer(self):
+        return self.__customer
 
 
-    '''
-    Hier erstellen Sie der Reihe nach die drei getter-Methoden.
-    Verwenden Sie dazu die Anotation @property.
-    '''
+    def booking(self, amount):
+        self.__balance += amount
 
-    '''
-    Erstellen Sie der Reihe nach die restlichen zwei Methoden.
-    '''
+    def get_money(self, amount):
+        if (self.__balance + self.__overdraft) > amount:
+            self.__balance -= amount
+            return amount
+        else:
+            return 0.0
+
+
+
+
