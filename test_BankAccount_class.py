2a22a7aa0d85440c2993b24ee5d3aaa152e05d0e
Rene Probst <rene.probst@bzz.ch>
2023-08-16 15:47:37 +0200

    task description added

--- test_BankAccount_class.py
+++ test_BankAccount_class.py
@@ -5,7 +5,7 @@ class TestBankAccount:
 
     @pytest.fixture
     def new_account(self):
-        return BankAccount(1000, "John Doe")
+        return BankAccount(1000, 'John Doe')
 
     def test_initial_balance(self, new_account):
         assert new_account.balance == 0.0
@@ -14,7 +14,7 @@ class TestBankAccount:
         assert new_account.overdraft == 1000
 
     def test_customer(self, new_account):
-        assert new_account.customer == "John Doe"
+        assert new_account.customer == 'John Doe'
 
     def test_booking(self, new_account):
         new_account.booking(500)
